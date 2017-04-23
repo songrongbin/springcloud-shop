0c373fe5344cbdec9641c5556240abc94432d5a3
songrongbin <srb.123@163.com>
2017-04-23 12:24:23 +0800

    修改pom依赖

--- eureka-client/src/main/java/com/bins/eurekaclient/Application.java
+++ eureka-client/src/main/java/com/bins/eurekaclient/Application.java
@@ -1,24 +1,21 @@
 package com.bins.eurekaclient;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by songrongbin on 2016/9/28.
  */
-@SpringBootApplication
-@RestController
-@EnableAutoConfiguration
+//@SpringBootApplication
+//@RestController
+//@EnableAutoConfiguration
 public class Application {
-    @RequestMapping("/")
+    //@RequestMapping("/")
     public String home() {
         return "Hello world";
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+
+        //new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 }
---
+++ eureka-server/src/main/java/com/bins/eureka/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.bins.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        //下面两行代码都可以用来启动
+        SpringApplication.run(ServerApplication.class, args);
+        //new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
