0b4184d4f658827d72b3ce810348c215845bca44
songrongbin <srb.123@163.com>
2017-07-27 20:16:26 +0800

    zuul

---
+++ eureka-gateway/src/main/java/com/bins/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.bins.gateway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by songrongbin on 2017/7/27.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
+    }
+
+}
