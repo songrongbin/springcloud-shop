5172209f93769ac9eb14192fcce13c83bea4fd13
andybin <srb.123@163.com>
2021-01-23 19:31:35 +0800

    shop

--- shop-user/src/main/java/com/bins/user/UserApplication.java
+++ shop-user/src/main/java/com/bins/user/UserApplication.java
@@ -2,7 +2,6 @@ package com.bins.user;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
